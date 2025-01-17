72537a64134782f8b57a2168b6190abfd482cd94
John Waite <mattekure@gmail.com>
2025-01-17 12:31:36 -0500

    add my files

---
+++ lua/plugins/xmlformatter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        xml = { "xmlformatter" },
+      },
+      formatters = {
+        xmlformatter = {
+          prepend_args = { "--preserve=script", "--indent=4", "--selfclose" },
+        },
+      },
+    },
+  },
+}
